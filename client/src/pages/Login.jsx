0cfbcae0e03cc326866a31a564ead65d0d68a74c
BheemaReddy07 <bheemareddy29102003@gmail.com>
2025-02-25 22:49:52 +0530

    login spin

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 import { AppContext } from "../context/AppContext";
-import {useNavigate} from 'react-router-dom'
-import {toast} from 'react-toastify'
-import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     document.body.style.overflow = "hidden";
 
@@ -14,7 +15,7 @@ const Login = () => {
     };
   }, []);
 
-  const { setShowLogin,token,setToken ,backendurl,} = useContext(AppContext);
+  const { setShowLogin, token, setToken, backendurl } = useContext(AppContext);
 
   const [state, setState] = useState("Login");
   const [forgotPasswordMode, setForgotPasswordMode] = useState(false);
@@ -28,21 +29,21 @@ const Login = () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const [showRePassword, setShowRePassword] = useState(false);
-   
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOTPRequest = async () => {
-    const loadingNotification = toast.loading("Sending OTP....");
-    setTimeout(() => {
-      toast.dismiss(loadingNotification);
-    }, 5000);
+    
     try {
+      setIsLoading(true);
       const endpoint =
         state === "Sign Up"
           ? "/api/user/register/request-otp"
           : "/api/user/forgot/request-otp";
 
       const payload =
-        state === "Sign Up" ? { name, email, password ,repassword} : { email,password,repassword };
+        state === "Sign Up"
+          ? { name, email, password, repassword }
+          : { email, password, repassword };
 
       const { data } = await axios.post(backendurl + endpoint, payload);
       if (data.success) {
@@ -54,12 +55,16 @@ const Login = () => {
     } catch (error) {
       toast.error(error.message);
     }
+    finally{
+      setIsLoading(false)
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
+      setIsLoading(true);
       if (state === "Sign Up") {
         const endpoint = otpSent
           ? "/api/user/register/verifyotp-register"
@@ -75,7 +80,7 @@ const Login = () => {
           if (otpSent) {
             localStorage.setItem("token", data.token);
             setToken(data.token);
-            setShowLogin(false)
+            setShowLogin(false);
           }
         } else {
           toast.error(data.message);
@@ -92,7 +97,7 @@ const Login = () => {
         const { data } = await axios.post(backendurl + endpoint, payload);
         if (data.success) {
           toast.success(data.message);
-          navigate("/")
+          navigate("/");
           if (otpSent) {
             setForgotPasswordMode(false); // This should disable Forgot Password mode after reset
             setOTPSent(false); // Reset OTP sent status after reset
@@ -112,23 +117,24 @@ const Login = () => {
           localStorage.setItem("token", data.token);
           setToken(data.token);
           toast.success(data.message);
-          setShowLogin(false)
-          navigate("/")
+          setShowLogin(false);
+          navigate("/");
         } else {
           toast.error(data.message);
         }
       }
     } catch (error) {
       toast.error(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className=" fixed inset-0 z-10 flex items-center justify-center backdrop-blur-sm bg-black/30">
-     <form
-        onSubmit={handleSubmit}  className="min-h-[80vh] flex items-center" >
-      <div className="bg-white flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg ">
-      <img
+      <form onSubmit={handleSubmit} className="min-h-[80vh] flex items-center">
+        <div className="bg-white flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg ">
+          <img
             src="cross_icon.svg"
             alt="Close"
             className="ml-80 w-3 h-3 cursor-pointer transition-transform transform hover:scale-110"
@@ -139,177 +145,202 @@ const Login = () => {
               setShowLogin(false);
             }}
           />
-        <p className="text-2xl font-semibold">
-          {state === "Sign Up"
-            ? "Create Account"
-            : forgotPasswordMode
+          <p className="text-2xl font-semibold">
+            {state === "Sign Up"
+              ? "Create Account"
+              : forgotPasswordMode
               ? "Forgot Password"
               : "Login"}
-        </p>
-        {state === "Sign Up" && (
+          </p>
+          {state === "Sign Up" && (
+            <div className="w-full">
+              <p>Full Name</p>
+              <input
+                className="border border-zinc-300 rounded w-full p-2 mt-1"
+                type="text"
+                value={name}
+                placeholder="Enter Name"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+          )}
+
           <div className="w-full">
-            <p>Full Name</p>
+            <p>Email</p>
             <input
               className="border border-zinc-300 rounded w-full p-2 mt-1"
-              type="text"
-              value={name}
-              placeholder="Enter Name"
-              onChange={(e) => setName(e.target.value)}
+              type="email"
+              value={email}
+              placeholder="Enter a Valid Email"
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
-        )}
 
-        <div className="w-full">
-          <p>Email</p>
-          <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="email"
-            value={email}
-            placeholder="Enter a Valid Email"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-
-        </div>
-
-        <div className="w-full relative">
-          <p className="text-sm text-gray-700">Password</p>
-          <div className="relative">
-            <input
-              className="border border-zinc-300 rounded w-full p-2 mt-1 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-              type={showPassword ? 'text' : 'password'}
-              value={password}
-              placeholder={forgotPasswordMode?"New Password":"Password"}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            {showPassword ? (
-              <EyeInvisibleOutlined
-                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
-                onClick={() => setShowPassword(false)}
-              />
-            ) : (
-              <EyeOutlined
-                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
-                onClick={() => setShowPassword(true)}
-              />
-            )}
-          </div>
-        </div>
-
-
-        {(state === "Sign Up" || forgotPasswordMode) && (
           <div className="w-full relative">
-            <p className="text-sm text-gray-700">Re-Password</p>
+            <p className="text-sm text-gray-700">Password</p>
             <div className="relative">
               <input
                 className="border border-zinc-300 rounded w-full p-2 mt-1 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                type={showRePassword ? 'text' : 'password'}
-                value={repassword}
-                placeholder={forgotPasswordMode?"New Re-Password":"Re-Password"}
-                onChange={(e) => setRePassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+                value={password}
+                placeholder={forgotPasswordMode ? "New Password" : "Password"}
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
-              {showRePassword ? (
+              {showPassword ? (
                 <EyeInvisibleOutlined
                   className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
-                  onClick={() => setShowRePassword(false)}
+                  onClick={() => setShowPassword(false)}
                 />
               ) : (
                 <EyeOutlined
                   className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
-                  onClick={() => setShowRePassword(true)}
+                  onClick={() => setShowPassword(true)}
                 />
               )}
             </div>
           </div>
 
-        )}
+          {(state === "Sign Up" || forgotPasswordMode) && (
+            <div className="w-full relative">
+              <p className="text-sm text-gray-700">Re-Password</p>
+              <div className="relative">
+                <input
+                  className="border border-zinc-300 rounded w-full p-2 mt-1 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  type={showRePassword ? "text" : "password"}
+                  value={repassword}
+                  placeholder={
+                    forgotPasswordMode ? "New Re-Password" : "Re-Password"
+                  }
+                  onChange={(e) => setRePassword(e.target.value)}
+                  required
+                />
+                {showRePassword ? (
+                  <EyeInvisibleOutlined
+                    className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+                    onClick={() => setShowRePassword(false)}
+                  />
+                ) : (
+                  <EyeOutlined
+                    className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+                    onClick={() => setShowRePassword(true)}
+                  />
+                )}
+              </div>
+            </div>
+          )}
 
-        {otpSent && (
-          <div className="w-full">
-            <p>OTP</p>
-            <input
-              className="border border-zinc-300 rounded w-full p-2 mt-1"
-              type="text"
-              value={otp}
-              placeholder="OTP"
-              onChange={(e) => setOTP(e.target.value)}
-              required
-            />
-          </div>
-        )}
+          {otpSent && (
+            <div className="w-full">
+              <p>OTP</p>
+              <input
+                className="border border-zinc-300 rounded w-full p-2 mt-1"
+                type="text"
+                value={otp}
+                placeholder="OTP"
+                onChange={(e) => setOTP(e.target.value)}
+                required
+              />
+            </div>
+          )}
 
-        {state === "Login" && !forgotPasswordMode && !otpSent && (
-          <button
-            type="submit"
-            className="bg-blue-700 text-white w-full py-2 rounded-md text-base"
-          >
-            Login
-          </button>
-        )}
+          {state === "Login" && !forgotPasswordMode && !otpSent && (
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-blue-700 text-white w-full py-2 rounded-md text-base flex items-center justify-center gap-2 disabled:opacity-50"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="h-5 w-5 animate-spin" />
+                  <span> Please Wait</span>
+                </>
+              ) : (
+                "Login"
+              )}
+            </button>
+          )}
 
-        {(state === "Sign Up" || forgotPasswordMode) && !otpSent && (
-          <button
-            type="button"
-            onClick={handleOTPRequest}
-            className="bg-blue-700 text-white w-full py-2 rounded-md text-base"
-          >
-            Send OTP
-          </button>
-        )}
+          {(state === "Sign Up" || forgotPasswordMode) && !otpSent && (
+            <button
+              type="button"
+              onClick={handleOTPRequest}
+              disabled={isLoading}
+              className="bg-blue-700 text-white w-full py-2 rounded-md text-base flex items-center justify-center gap-2 disabled:opacity-50"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="h-5 w-5 animate-spin" />
+                  <span> Please Wait</span>
+                </>
+              ) : (
+                "Send OTP"
+              )}
+            </button>
+          )}
 
-        {otpSent && (
-          <button
-            type="submit"
-            className="bg-blue-700 text-white w-full py-2 rounded-md text-base"
-          >
-            {forgotPasswordMode ? "Reset Password" : "Create Account"}
-          </button>
-        )}
+          {otpSent && (
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-blue-700 text-white w-full py-2 rounded-md text-base flex items-center justify-center gap-2 disabled:opacity-50"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="h-5 w-5 animate-spin" />
+                  <span> Please Wait</span>
+                </>
+              ) : forgotPasswordMode ? (
+                "Reset Password"
+              ) : (
+                "Create Account"
+              )}
+            </button>
+          )}
 
-        {!forgotPasswordMode && state !== "Sign Up" && (
-          <button
-            type="button"
-            onClick={() => setForgotPasswordMode(true)}
-            className="text-primary underline cursor-pointer"
-          >
-            Forgot Password?
-          </button>
-        )}
-
-        {forgotPasswordMode || state === "Sign Up" ? (
-          <p>
-            Already have an account?{" "}
-            <span
-              onClick={() => {
-                setState("Login");
-                setForgotPasswordMode(false);
-                setOTPSent(false);
-              }}
+          {!forgotPasswordMode && state !== "Sign Up" && (
+            <button
+              type="button"
+              onClick={() => setForgotPasswordMode(true)}
               className="text-primary underline cursor-pointer"
             >
-              Login here
-            </span>
-          </p>
-        ) : (
-          <p>
-            Create a new account?{" "}
-            <span
-              onClick={() => {
-                setState("Sign Up");
-                setForgotPasswordMode(false);
-                setOTPSent(false);
-              }}
-              className="text-primary underline cursor-pointer"
-            >
-              Click here
-            </span>
-          </p>
-        )}
-      </div>
-    </form>
+              Forgot Password?
+            </button>
+          )}
+
+          {forgotPasswordMode || state === "Sign Up" ? (
+            <p>
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  setState("Login");
+                  setForgotPasswordMode(false);
+                  setOTPSent(false);
+                }}
+                className="text-primary underline cursor-pointer"
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p>
+              Create a new account?{" "}
+              <span
+                onClick={() => {
+                  setState("Sign Up");
+                  setForgotPasswordMode(false);
+                  setOTPSent(false);
+                }}
+                className="text-primary underline cursor-pointer"
+              >
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
+      </form>
     </div>
   );
 };
